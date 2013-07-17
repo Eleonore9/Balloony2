0b4125dd22c7b2e83e37cc518a3af478bb5698c7
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-17 21:58:58 +0200

    Balloony moves and clouds are fine

--- static/js/ballony.js
+++ static/js/ballony.js
@@ -11,16 +11,16 @@ $(document).ready(function() {
         switch(parseInt(key.which,10)) {
             case 76:
             	// Add a condition on Ballony current position
-                $('.balloony').animate({left: "-=200px"}, 'fast');
+                $('.balloony').animate({left: "-=100px"}, 'slow');
 				break;
 			case 68:
-				$('.balloony').animate({top: "+=200px"}, 'fast');
+				$('.balloony').animate({top: "+=100px"}, 'slow');
 				break;
 			case 85:
-				$('.balloony').animate({top: "-=200px"}, 'fast');
+				$('.balloony').animate({top: "-=100px"}, 'slow');
 				break;
 			case 82:
-				$('.balloony').animate({left: "+=200px"}, 'fast');
+				$('.balloony').animate({left: "+=100px"}, 'slow');
 				break;
 			default:
 				break;
