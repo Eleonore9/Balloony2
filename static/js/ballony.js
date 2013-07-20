f2be8cb6f2fabe580d253811108a038d22379b10
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-21 00:04:28 +0200

    adding a two elements jquery animation, pb w the condition

--- static/js/ballony.js
+++ static/js/ballony.js
@@ -1,29 +1,39 @@
-// $(document).ready(function() {
-// 	var game = require('game');
-// 	var ballonyGame = new game.board(5, 3);
-// 	console.log(ballonyGame.width + " - " + ballonyGame.height);
-// 	ballonyGame.setBoard($('.content'));
-// })
-
 $(document).ready(function() {
     $(document).keydown(function(key) {
     	// Use keys to move Ballony around
         switch(parseInt(key.which,10)) {
             case 76:
-            	// Add a condition on Ballony current position
                 $('.balloony').animate({left: "-=100px"}, 'slow');
-				break;
-			case 68:
-				$('.balloony').animate({top: "+=100px"}, 'slow');
-				break;
-			case 85:
-				$('.balloony').animate({top: "-=100px"}, 'slow');
-				break;
-			case 82:
-				$('.balloony').animate({left: "+=100px"}, 'slow');
-				break;
-			default:
-				break;
-		}
+		break;
+	    case 68:
+		$('.balloony').animate({top: "+=100px"}, 'slow');
+		break;
+	    case 85:
+		$('.balloony').animate({top: "-=100px"}, 'slow');
+		break;
+	    case 82:
+		$('.balloony').animate({left: "+=100px"}, 'slow');
+		break;
+	    default:
+	        break;
+	    }
 	});
+    // This idea is to move Balloony and the cloud when Balloony is on a cloud
+    // initial position is 380 for left and 320 for top
+    // after 2 clicks (200 px), Ballony is on the first cloud
+    // I then when both to be animated together
+    var balloony = $('.balloony');
+    var position = balloony.position();
+    //$('span').text('left: ' + position.left + ', top: ' + position.top);
+    if(position.left === 580 && position.top === 320) {
+	$(document).keydown(function(key) {
+	   switch(parseInt(key.which,10)) {
+	        case 76:
+	            $('.balloony, .cloud.first').animate({left: "-=100px"}, 'slow');
+		    break;
+		case 82:
+		    $('.balloony, .cloud.first').animate({left: "+=100px"}, 'slow');
+		    break;
+	     }
+	 });
 });
