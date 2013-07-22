97081b8a2801facba98668e71a9b7a412dba89a4
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-22 21:44:44 +0200

    still not working as element position is not updated after animation

--- static/js/ballony.js
+++ static/js/ballony.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
-    var balloony = $('.balloony');
-    var position = balloony.position();
+    var ball_left = $('.balloony').offset().left;
+    var ball_top = $('.balloony').offset().top;
+    $('span').html("Ballony - left: " + ball_left + ", top: " + ball_top);
+    var grid_left = $('.grid.first_grid').offset().left;
+    var grid_top = $('.grid.first_grid').offset().top;
+    $('span').append("Grid - left: " + grid_left + ", top: " + grid_top);
     $(document).keydown(function(key) {
     	// Use keys to move Ballony around
         switch(parseInt(key.which,10)) {
             case 76:
-    		if(position.left === 580 && position.top === 320) {
+    		if(ball_left === 580 && ball_top === 320) {
 			$('.cloud:nth-of-type(1)').addClass('noanimation');
 	        	$('.balloony, .cloud:nth-of-type(1)').animate({left: "-=100px"}, 'slow');
-	        	//$('.balloony, .cloud.first').animate({left: "-=100px"}, 'slow');
+			//$('.balloony, .cloud.first').animate({left: "-=100px"}, 'slow');
+			break;
 		} else {   
-                $('.balloony').animate({left: "-=100px"}, 'slow');}
-		break;
+                $('.balloony').animate({left: "-=100px"}, 'slow');
+    		$('span').html("left: " + ball_left + ", top: " + ball_top);
+		break;}
 	    case 68:
 		$('.balloony').animate({top: "+=100px"}, 'slow');
+   		$('span').html("left: " + ball_left + ", top: " + ball_top);
 		break;
 	    case 85:
 		$('.balloony').animate({top: "-=100px"}, 'slow');
+    		$('span').html("left: " + ball_left + ", top: " + ball_top);
 		break;
 	    case 82:
-    		if(position.left === 580 && position.top === 320) {
+    		if(ball_left === 580 && ball_top === 320) {
 			$('.cloud:nth-of-type(1)').addClass('noanimation');
 		        $('.balloony, .cloud:nth-of-type(1)').animate({left: "+=100px"}, 'slow');
 		        //$('.balloony, .cloud.first').animate({left: "+=100px"}, 'slow');
+			break;
 		} else {   
-		$('.balloony').animate({left: "+=100px"}, 'slow');}
-		break;
+		$('.balloony').animate({left: "+=100px"}, 'slow');
+    		$('span').html("left: " + ball_left + ", top: " + ball_top);
+		break;}
 	    default:
 	        break;
 	    }
